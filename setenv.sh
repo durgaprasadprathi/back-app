2474736d957933778dcd52a33da99331b44e1c25
durgaprasadprathi <49200883+durgaprasadprathi@users.noreply.github.com>
2021-10-31 22:58:05 +0530

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export JDBC_DRIVER="org.postgresql.Driver"
-export JDBC_URL="jdbc:postgresql://3.136.66.53:5432/exec_module"
+export JDBC_URL="jdbc:postgresql://172.31.27.194:5432/exec_module"
 export POSTGRES_USERNAME="exec_module"
 export POSTGRES_PASSWORD="exec_module1010#"
 export WORKING_DIR="/home/execution-module/"
-export TERRAFORM_POSTGRES_CONN_STR="postgres://exec_module1:exec_module1010@3.136.66.53:5432/exec_module?sslmode=disable"
+export TERRAFORM_POSTGRES_CONN_STR="postgres://exec_module1:exec_module1010@172.31.27.194:5432/exec_module?sslmode=disable"
 export PULUMI_BASE_URL="http://172.31.20.143:3000/"
